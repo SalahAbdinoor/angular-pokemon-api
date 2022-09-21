2b913cbcfc20ab6288e84b7e306fe2ff25324a81
Salah Abdinoor <12002329-SalahAbdinoor@users.noreply.gitlab.com>
2022-09-21 21:37:17 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiTrainers: 'https://sign-language-translation-asl.herokuapp.com/trainers',
+  apiKey: 'mcxjgprvtpryfympjuuhmjmzqngrtmkdjkwhwhemmsnyvaanypwjvkuusrsjnveb',
+  apiPokemons: 'https://pokeapi.co/api/v2/pokemon',
 };
